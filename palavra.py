0efcc8cf959a264a1cd04920bbb3fe7bc197eb68
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-12-15 19:44:29 -0300

    Add files via upload

---
+++ palavra.py
@@ -0,0 +1,12 @@
+# quantos caracteres determinada palavra tem
+
+word = str(input("Digite uma palavra: "))
+lista = list(word)
+print(f"A palavra {word} tem {len(word)} caracteres")
+
+# OU (usando a ferramenta de loop)
+
+i = 0
+for letra in word:
+    i +=1
+print(f"A palavra {word} tem {i} caracteres")
